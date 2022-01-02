a76103bb25ad27b82253951f9a96fc0404051a68
RJ Smith <rj@rjsmith.me>
2022-01-01 20:13:40 -0500

    Added discord invite command logic.

---
+++ src/commands/discordInvite.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('invite')
+    .setDescription('Provides an invite to the Wildcard discord server.'),
+  async execute(interaction) {
+    await interaction.reply('https://discord.gg/jftcgfP');
+  },
+};
