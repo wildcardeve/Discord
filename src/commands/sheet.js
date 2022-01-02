ad23c8ad93ec5be74d815b873d23ad01a150cf18
RJ Smith <rj@rjsmith.me>
2022-01-01 22:47:29 -0500

    Added logic for sheet command.

---
+++ src/commands/sheet.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('sheet')
+    .setDescription('Provides a link to the doctrine sheet.'),
+  async execute(interaction) {
+    await interaction.reply('https://docs.google.com/spreadsheets/d/1vQh1yvvaL0RiezbaPyfutnEQ9CzgBhS-dTHOB9lYcc8/edit?usp=sharing');
+  },
+};
