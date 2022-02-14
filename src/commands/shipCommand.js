58222bbd0a62c172ba3d7ac2044add5504bdc7ea
RJ Smith <rj@rjsmith.me>
2022-02-13 19:32:56 -0500

    Added a command to randomly select a ship to fly.

---
+++ src/commands/shipCommand.js
@@ -0,0 +1,49 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const logger = require('../utils/logger');
+
+const options = [
+  'Chair is a big dumb idiot',
+  'Sleipnir',
+  'Nighthawk',
+  'Retribution',
+  'Oracle',
+  'Orthrus',
+  'Hurricane',
+  'Hurricane Fleet Issue',
+  'Drake Navy Issue',
+  'Caracal',
+  'Nightmare',
+  'Interceptor',
+  'Phantasm',
+  'Loki',
+  'Vedmak',
+  'Draugur',
+  'Drekavak',
+  'Deez Nuts',
+  'Svipul',
+  'Stabber',
+  'Vagabond',
+  'Omen Navy Issue',
+  'Osprey Navy Issue',
+  'Vargur',
+  'Typhoon',
+  'Typhoon Fleet Issue',
+  'Tempest',
+  'Legion',
+  'Zealot',
+  'Harm Egg',
+  'Machariel',
+];
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('ship')
+    .setDescription('Choose a ship to fly.')
+    .addStringOption((option) => option.setRequired(true).setName('question').setDescription('This command will pick a ship for you to fly at random.')),
+  async execute(interaction) {
+    // eslint-disable-next-line no-underscore-dangle
+    const { value } = interaction.options._hoistedOptions[0];
+    logger.info(`Processing question "${value}" for an answer`);
+    await interaction.reply(`You should fly: ${options[Math.floor(Math.random() * options.length)]}`);
+  },
+};
