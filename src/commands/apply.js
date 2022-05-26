847a1946c6c2025ac020ea4f94fe54755d05a554
Michael  Stack <mstack000@gmail.com>
2022-05-26 14:04:16 -0400

    fixed?

--- src/commands/apply.js
+++ src/commands/apply.js
@@ -2,55 +2,53 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 const { Permissions } = require("discord.js");
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("apply")
-    .setDescription(
-      "Makes a room for the potential applicant. (requires recruiter permissions"
-    )
-    .addUserOption((option) =>
-      option.setName("user").setDescription("The user").setRequired(true)
-    ),
-  async execute(interaction) {
-    const user = interaction.options._hoistedOptions[0];
-    const recruiter = "737696484866392165";
-    const command = "752710795623923812";
-    const guildRef = interaction.guild;
-    const basePermissions = [
-      Permissions.FLAGS.VIEW_CHANNEL,
-      Permissions.FLAGS.SEND_MESSAGES,
-      Permissions.FLAGS.ADD_REACTIONS,
-      Permissions.FLAGS.READ_MESSAGE_HISTORY,
-      Permissions.FLAGS.EMBED_LINKS,
-    ];
+    data: new SlashCommandBuilder()
+        .setName("apply")
+        .setDescription(
+            "Makes a room for the potential applicant. (requires recruiter permissions)"
+        )
+        .addUserOption((option) =>
+            option.setName("user").setDescription("The user").setRequired(true)
+        ),
+    async execute(interaction) {
+        const user = interaction.options._hoistedOptions[0];
+        const recruiter = "737696484866392165";
+        const command = "752710795623923812";
+        const guildRef = interaction.guild_id;
+        const basePermissions = [
+            Permissions.FLAGS.VIEW_CHANNEL,
+            Permissions.FLAGS.SEND_MESSAGES,
+            Permissions.FLAGS.ADD_REACTIONS,
+            Permissions.FLAGS.READ_MESSAGE_HISTORY,
+            Permissions.FLAGS.EMBED_LINKS,
+        ];
 
-    await interaction(
-      guildRef.channels.permissionOverwrites.create(
-        `${user}-applicant-meeting`,
-        {
-          category: "RECRUITMENT",
-          permissionOverwrites: [
-            {
-              id: guildRef.id,
-              deny: [Permissions.FLAGS.VIEW_CHANNEL],
-            },
-            {
-              id: user.id,
-              allow: basePermissions,
-            },
-            {
-              id: recruiter,
-              allow: [...basePermissions, Permissions.FLAGS.MANAGE_CHANNEL],
-            },
-            {
-              id: command,
-              allow: [...basePermissions, Permissions.FLAGS.MANAGE_CHANNEL],
-            },
-          ],
-        }
-      ),
-      interaction.message.cache
-        .get(`${user}-applicant-meeting`)
-        .send(`https://wildcardeve.com/`)
-    );
-  },
+        await interaction(
+            guildRef.channels.permissionOverwrites.create(
+                `${user}-applicant-meeting`, {
+                    category: "RECRUITMENT",
+                    permissionOverwrites: [{
+                            id: guildRef,
+                            deny: [Permissions.FLAGS.VIEW_CHANNEL],
+                        },
+                        {
+                            id: user.id,
+                            allow: basePermissions,
+                        },
+                        {
+                            id: recruiter,
+                            allow: [...basePermissions, Permissions.FLAGS.MANAGE_CHANNEL],
+                        },
+                        {
+                            id: command,
+                            allow: [...basePermissions, Permissions.FLAGS.MANAGE_CHANNEL],
+                        },
+                    ],
+                }
+            ),
+            interaction.message.cache
+            .get(`${user}-applicant-meeting`)
+            .send(`https://wildcardeve.com/`)
+        );
+    },
 };
