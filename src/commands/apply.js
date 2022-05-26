20a07b41831c1ca75350081f940e0b8e417da39a
Michael  Stack <mstack000@gmail.com>
2022-05-26 13:56:23 -0400

    small changes (pls work)
    identify guild from interactions data model

--- src/commands/apply.js
+++ src/commands/apply.js
@@ -7,40 +7,36 @@ module.exports = {
         .setDescription('Makes a room for the potential applicant. (requires recruiter permissions')
         .addUserOption(option => option.setName('user').setDescription('The user').setRequired(true)),
     async execute(interaction) {
+        const guildId = interaction.guild_id
         const user = interaction.options._hoistedOptions[0]
         const recruiter = '737696484866392165'
         const command = '752710795623923812'
+        const STANDARD_PERMS = {
+            allow: Permissions.FLAGS.VIEW_CHANNEL,
+            allow: Permissions.FLAGS.SEND_MESSAGES,
+            allow: Permissions.FLAGS.ADD_REACTIONS,
+            allow: Permissions.FLAGS.READ_MESSAGE_HISTORY,
+            allow: Permissions.FLAGS.EMBED_LINKS
+        }
         await interaction(
-            guild.channels.permissionOverwrites.create(`${user}-applicant-meeting`, {
+            guildId.channels.permissionOverwrites.create(`${user}-applicant-meeting`, {
                 category: 'RECRUITMENT',
                 permissionOverwrites: [{
-                        id: guild.id,
+                        id: guildId,
                         deny: [Permissions.FLAGS.VIEW_CHANNEL]
                     },
                     {
                         id: user.id,
-                        allow: Permissions.FLAGS.VIEW_CHANNEL,
-                        allow: Permissions.FLAGS.SEND_MESSAGES,
-                        allow: Permissions.FLAGS.ADD_REACTIONS,
-                        allow: Permissions.FLAGS.READ_MESSAGE_HISTORY,
-                        allow: Permissions.FLAGS.EMBED_LINKS
+                        STANDARD_PERMS
                     },
                     {
                         id: recruiter,
-                        allow: Permissions.FLAGS.VIEW_CHANNEL,
-                        allow: Permissions.FLAGS.SEND_MESSAGES,
-                        allow: Permissions.FLAGS.ADD_REACTIONS,
-                        allow: Permissions.FLAGS.READ_MESSAGE_HISTORY,
-                        allow: Permissions.FLAGS.EMBED_LINKS,
+                        STANDARD_PERMS,
                         allow: Permissions.FLAGS.MANAGE_CHANNEL
                     },
                     {
                         id: command,
-                        allow: Permissions.FLAGS.VIEW_CHANNEL,
-                        allow: Permissions.FLAGS.SEND_MESSAGES,
-                        allow: Permissions.FLAGS.ADD_REACTIONS,
-                        allow: Permissions.FLAGS.READ_MESSAGE_HISTORY,
-                        allow: Permissions.FLAGS.EMBED_LINKS,
+                        STANDARD_PERMS,
                         allow: Permissions.FLAGS.MANAGE_CHANNEL
                     }
                 ]
