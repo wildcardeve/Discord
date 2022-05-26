ce85d46060a67b9456bf734ed8b9a1a7761339c2
RJ <rj@rjsmith.me>
2022-05-26 08:28:58 -0400

    Lets see hmm

--- src/commands/apply.js
+++ src/commands/apply.js
@@ -1,51 +1,56 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
+const { Permissions } = require("discord.js");
 
 module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("apply")
+    .setDescription(
+      "Makes a room for the potential applicant. (requires recruiter permissions"
+    )
+    .addUserOption((option) =>
+      option.setName("user").setDescription("The user").setRequired(true)
+    ),
+  async execute(interaction) {
+    const user = interaction.options._hoistedOptions[0];
+    const recruiter = "737696484866392165";
+    const command = "752710795623923812";
+    const guildRef = interaction.guild;
+    const basePermissions = [
+      Permissions.FLAGS.VIEW_CHANNEL,
+      Permissions.FLAGS.SEND_MESSAGES,
+      Permissions.FLAGS.ADD_REACTIONS,
+      Permissions.FLAGS.READ_MESSAGE_HISTORY,
+      Permissions.FLAGS.EMBED_LINKS,
+    ];
 
-    data: new SlashCommandBuilder()
-        .setName('apply')
-        .setDescription('Makes a room for the potential applicant. (requires recruiter permissions')
-        .addUserOption(option => option.setName('user').setDescription('The user').setRequired(true)),
-    async execute(interaction) {
-        const user = interaction.options._hoistedOptions[0]
-        const recruiter = '737696484866392165'
-        const command = '752710795623923812'
-        await interaction(
-            guild.channels.permissionOverwrites.create(`${user}-applicant-meeting`, {
-                category: 'RECRUITMENT',
-                permissionOverwrites: [{
-                        id: guild.id,
-                        deny: [Permissions.FLAGS.VIEW_CHANNEL]
-                    },
-                    {
-                        id: user.id,
-                        allow: Permissions.FLAGS.VIEW_CHANNEL,
-                        allow: Permissions.FLAGS.SEND_MESSAGES,
-                        allow: Permissions.FLAGS.ADD_REACTIONS,
-                        allow: Permissions.FLAGS.READ_MESSAGE_HISTORY,
-                        allow: Permissions.FLAGS.EMBED_LINKS
-                    },
-                    {
-                        id: recruiter,
-                        allow: Permissions.FLAGS.VIEW_CHANNEL,
-                        allow: Permissions.FLAGS.SEND_MESSAGES,
-                        allow: Permissions.FLAGS.ADD_REACTIONS,
-                        allow: Permissions.FLAGS.READ_MESSAGE_HISTORY,
-                        allow: Permissions.FLAGS.EMBED_LINKS,
-                        allow: Permissions.FLAGS.MANAGE_CHANNEL
-                    },
-                    {
-                        id: command,
-                        allow: Permissions.FLAGS.VIEW_CHANNEL,
-                        allow: Permissions.FLAGS.SEND_MESSAGES,
-                        allow: Permissions.FLAGS.ADD_REACTIONS,
-                        allow: Permissions.FLAGS.READ_MESSAGE_HISTORY,
-                        allow: Permissions.FLAGS.EMBED_LINKS,
-                        allow: Permissions.FLAGS.MANAGE_CHANNEL
-                    }
-                ]
-            }), newChan = clients.channels.cache.get(`${user}-applicant-meeting`),
-            message.newChan.send(`https://wildcardeve.com/`)
-        )
-    }
+    await interaction(
+      guildRef.channels.permissionOverwrites.create(
+        `${user}-applicant-meeting`,
+        {
+          category: "RECRUITMENT",
+          permissionOverwrites: [
+            {
+              id: guildRef.id,
+              deny: [Permissions.FLAGS.VIEW_CHANNEL],
+            },
+            {
+              id: user.id,
+              allow: basePermissions,
+            },
+            {
+              id: recruiter,
+              allow: [...basePermissions, Permissions.FLAGS.MANAGE_CHANNEL],
+            },
+            {
+              id: command,
+              allow: [...basePermissions, Permissions.FLAGS.MANAGE_CHANNEL],
+            },
+          ],
+        }
+      ),
+      interaction.message.cache
+        .get(`${user}-applicant-meeting`)
+        .send(`https://wildcardeve.com/`)
+    );
+  },
 };
