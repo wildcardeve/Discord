636e31d56c1a6cf7aa873cb2c6f431e7339b5b87
Michael  Stack <mstack000@gmail.com>
2022-05-26 17:19:31 -0400

    fixed

--- src/commands/apply.js
+++ src/commands/apply.js
@@ -1,5 +1,4 @@
-const { SlashCommandBuilder } = require("@discordjs/builders");
-const { Permissions } = require("discord.js");
+const { SlashCommandBuilder } = require("@discordjs/builders")
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -12,39 +11,14 @@ module.exports = {
         ),
     async execute(interaction) {
         const user = interaction.options.getUser('user');
-        const everyone = "737695395794256023"
-        const recruiter = "737696484866392165";
-        const command = "752710795623923812";
-        const basePermissions = [
-            Permissions.FLAGS.VIEW_CHANNEL,
-            Permissions.FLAGS.SEND_MESSAGES,
-            Permissions.FLAGS.ADD_REACTIONS,
-            Permissions.FLAGS.READ_MESSAGE_HISTORY,
-            Permissions.FLAGS.EMBED_LINKS,
-        ];
 
         await interaction(
             interaction.guild.channels.create(
                 `${user.username}-applicant-meeting`, {
-                    permissionOverwrites: [{
-                            id: everyone,
-                            deny: [Permissions.FLAGS.VIEW_CHANNEL]
-                        },
-                        {
-                            id: user,
-                            allow: [basePermissions, Permissions.FLAGS.VIEW_CHANNEL]
-                        },
-                        {
-                            id: recruiter,
-                            allow: [basePermissions, Permissions.FLAGS.VIEW_CHANNEL]
-                        },
-                        {
-                            id: command,
-                            allow: [basePermissions, Permissions.FLAGS.VIEW_CHANNEL]
-                        },
-                    ]
+                    parent: '767531267641180180',
                 }
-            ).then(channel => channel.send(`Please apply on our website!\n
+            ).then(channel => channel.send(`\n
+            Please apply on our website!\n
             Ping @here when you are ready to have your application reviewed.\n
             https://wildcardeve.com/`)),
         );
